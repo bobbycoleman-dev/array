24af15a3a6b1699676c91f304ccb53f72e403f7d
Bobby Coleman <bobbycoleman.dev@gmail.com>
2023-09-20 12:06:54 -0500

    removed firestore db

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
@@ -15,6 +14,3 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const db = getFirestore(app);
-
-export default app;
